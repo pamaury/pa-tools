374739b40014ddf3695944069c01d4aaf830c755
Amaury Pouly <amaury.pouly@gmail.com>
2011-04-16 12:56:32 +0200

    derandomizer: implement time interception

--- derandomizer/derandomizer.c
+++ derandomizer/derandomizer.c
@@ -43,13 +43,13 @@ ssize_t read_user_mem(arg_t addr, char *buf, int len)
         mem_fd = open(memname, O_RDONLY);
         if(mem_fd < 0)
         {
-            perror("open(mem,rd) failed\n");
+            perror("[derandomizer] open(mem,rd) failed\n");
             exit(1);
         }
     }
     if(lseek64(mem_fd, addr, SEEK_SET) == (off64_t)-1)
     {
-        perror("lseek64(mem,rd) failed");
+        perror("[derandomizer] lseek64(mem,rd) failed");
         exit(1);
     }
     return read(mem_fd, buf, len);
@@ -80,39 +80,107 @@ ssize_t put_user_mem(arg_t addr, char *buf, int len)
     return cpy_len;
 }
 
+void exec_child(int argc, char **argv)
+{
+    (void) argc;
+    execve(argv[0], argv, NULL);
+}
+
 int main(int argc, char **argv)
 {
-    int log_fd;
-    bool intercept_read = false;
-    bool intercept_write = false;
+    int rand_read_fd = -1;
+    bool provide_rand = false;
+    int rand_write_fd = -1;
+    bool log_rand = false;
+    int time_read_fd = -1;
+    bool provide_time = false;
+    int time_write_fd = -1;
+    bool log_time = false;
     int dev_random_fd = -1;
     /* the first syscall is exec, which exists at the end, so from out point of view,
      * the first syscall is an exit because the next syscall is an actual entry */
     bool syscall_entry = false;
 
-    if(argc < 4)
+    if(argc < 2)
     {
-        printf("usage: %s <opt> <file> <program> [<args>]\n", argv[0]);
+        printf("usage: %s <opt> <program> [<args>]\n", argv[0]);
         printf("options:\n");
-        printf("\t-r\tRead random data from file\n");
-        printf("\t-w\tWrite random data to file\n");
+        printf("\t-r <file>\tRead random data from <file>\n");
+        printf("\t-w <file>\tWrite random data to <file>\n");
+        printf("\t-rt <file>\tRead time data from <file>\n");
+        printf("\t-wt <file>\tWrite time data to <file>\n");
         return 1;
     }
 
-    if(strcmp(argv[1], "-r") == 0)
-        intercept_read = true;
-    else if(strcmp(argv[1], "-w") == 0)
-        intercept_write = true;
-    else
+    argc--;
+    argv++;
+
+    while(argc >= 2)
     {
-        printf("Unknown option '%s'\n", argv[1]);
+        if(strcmp(argv[0], "-r") == 0)
+        {
+            rand_read_fd = open(argv[1], O_RDONLY);
+            if(rand_read_fd < 0)
+            {
+                perror("[derandomizer] Cannot open input random data file");
+                return 1;
+            }
+            argc -= 2;
+            argv += 2;
+            provide_rand = true;
+        }
+        else if(strcmp(argv[0], "-w") == 0)
+        {
+            rand_write_fd = open(argv[1], O_CREAT|O_WRONLY|O_TRUNC,
+                S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+            if(rand_write_fd < 0)
+            {
+                perror("[derandomizer] Cannot open output random data file");
+                return 1;
+            }
+            argc -= 2;
+            argv += 2;
+            log_rand = true;
+        }
+        else if(strcmp(argv[0], "-rt") == 0)
+        {
+            time_read_fd = open(argv[1], O_RDONLY);
+            if(time_read_fd < 0)
+            {
+                perror("[derandomizer] Cannot open input time data file");
+                return 1;
+            }
+            argc -= 2;
+            argv += 2;
+            provide_time = true;
+        }
+        else if(strcmp(argv[0], "-wt") == 0)
+        {
+            time_write_fd = open(argv[1], O_CREAT|O_WRONLY|O_TRUNC,
+                S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+            if(time_write_fd < 0)
+            {
+                perror("[derandomizer] Cannot open output time data file");
+                return 1;
+            }
+            argc -= 2;
+            argv += 2;
+            log_time = true;
+        }
+        else
+            break;
+    }
+
+    if(argc == 0)
+    {
+        printf("[derandomizer] Invalid command line\n");
         return 1;
     }
 
     struct utsname uts;
     if(uname(&uts) < 0)
     {
-        perror("uname failed\n");
+        perror("[derandomizer] uname failed\n");
         return 1;
     }
     #if __WORDSIZE == 64
@@ -123,7 +191,7 @@ int main(int argc, char **argv)
     #error error
     #endif
     {
-        printf("Program compiled for %s but running machine is %s\n",
+        printf("[derandomizer] Program compiled for %s but running machine is %s\n",
         #if __WORDSIZE == 64
             "x86_64",
         #else
@@ -133,23 +201,10 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    int flags = 0;
-    if(intercept_read)
-        flags |= O_RDONLY;
-    if(intercept_write)
-        flags |= O_CREAT|O_WRONLY|O_TRUNC;
-    log_fd = open(argv[2], flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    if(log_fd < 0)
-    {
-        perror("Cannot open random data file");
-        return 1;
-    }
-
     child_pid = fork();
     if(child_pid < 0)
     {
         perror("fork failed");
-        close(log_fd);
         return 1;
     }
 
@@ -157,12 +212,12 @@ int main(int argc, char **argv)
     {
         if(ptrace(PTRACE_TRACEME, 0, NULL, NULL) < 0)
         {
-            perror("ptrace(traceme) failed\n");
+            perror("[derandomizer] ptrace(traceme) failed\n");
             return 1;
         }
         /* make sure the other process is ready */
         raise(SIGSTOP);
-        execve(argv[3], &argv[4], NULL);
+        exec_child(argc, argv);
         return 1;
     }
 
@@ -173,7 +228,7 @@ int main(int argc, char **argv)
         pid_t p = wait4(child_pid, &stat, WUNTRACED, &rus);
         if(p < 0 || p != child_pid)
         {
-            perror("wait4 failed or bugged");
+            perror("[derandomizer] wait4 failed or bugged");
             break;
         }
         
@@ -189,7 +244,7 @@ int main(int argc, char **argv)
         struct user_regs_struct regs;
         if(ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) < 0)
         {
-            perror("ptrace(getregs) failed\n");
+            perror("[derandomizer] ptrace(getregs) failed\n");
             break;
         }
 
@@ -208,7 +263,7 @@ int main(int argc, char **argv)
         #endif
         if(syscall == __NR_open)
         {
-            if(syscall_entry && intercept_read)
+            if(syscall_entry && provide_rand)
             {
                 char buf[128];
                 if(read_user_mem(arg1, buf, 128) == 128 && strcmp(buf, "/dev/random") == 0)
@@ -227,17 +282,17 @@ int main(int argc, char **argv)
                 if(read_user_mem(arg1, buf, 128) == 128 && strcmp(buf, "/dev/random") == 0)
                 {
                     if(dev_random_fd != -1)
-                        printf("[devrandomizer] /dev/random opened several times !\n");
+                        printf("[derandomizer] /dev/random opened several times !\n");
                     dev_random_fd = result;
                 }
             }
         }
         else if(syscall == __NR_read)
         {
-            if(arg1 == (arg_t)dev_random_fd && intercept_read && !syscall_entry)
+            if(arg1 == (arg_t)dev_random_fd && provide_rand && !syscall_entry)
             {
                 char *data = malloc(arg3);
-                if(data && read(log_fd, data, arg3) == (ssize_t)arg3 &&
+                if(data && read(rand_read_fd, data, arg3) == (ssize_t)arg3 &&
                         put_user_mem(arg2, data, arg3) == (ssize_t)arg3)
                     result = arg3;
                 else
@@ -250,16 +305,16 @@ int main(int argc, char **argv)
                 #endif
                 if(ptrace(PTRACE_SETREGS, child_pid, NULL, &regs) < 0)
                 {
-                    perror("ptrace(setregs) failed\n");
+                    perror("[derandomizer] ptrace(setregs) failed\n");
                     break;
                 }
             }
-            if(arg1 == (arg_t)dev_random_fd && intercept_write && !syscall_entry)
+            if(arg1 == (arg_t)dev_random_fd && log_rand && !syscall_entry)
             {
                 char *data = malloc(arg3);
                 if(data && read_user_mem(arg2, data, arg3) == (ssize_t)arg3)
-                    if(write(log_fd, data, arg3) != (ssize_t)arg3)
-                        printf("[devrandomizer] log write failure\n");
+                    if(write(rand_write_fd, data, arg3) != (ssize_t)arg3)
+                        printf("[derandomizer] Failure to log random data\n");
                 free(data);
             }
         }
@@ -268,11 +323,35 @@ int main(int argc, char **argv)
             if(arg1 == (arg_t)dev_random_fd)
                 dev_random_fd = -1;
         }
+        else if(syscall == __NR_time && !syscall_entry)
+        {
+            if(log_time)
+            {
+                if(write(time_write_fd, &result, sizeof result) != sizeof result)
+                    printf("[derandomizer] Failure to log time data\n");
+            }
+            if(provide_time)
+            {
+                if(read(time_read_fd, &result, sizeof result) != sizeof result)
+                    result = -1;
+                if(arg1 != 0 && put_user_mem(arg1, (char *)&result, sizeof result) != sizeof result)
+                    result = -1;
+                #if __WORDSIZE == 64
+                regs.rax = result;
+                #elif __WORDSIZE == 32
+                regs.eax = result;
+                #endif
+                if(ptrace(PTRACE_SETREGS, child_pid, NULL, &regs) < 0)
+                {
+                    perror("[derandomizer] ptrace(setregs) failed\n");
+                    break;
+                }
+            }
+        }
 
         syscall_entry = !syscall_entry;
         ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL);
     }
 
-    close(log_fd);
     return 0;
 }
--- derandomizer/unittest.c
+++ derandomizer/unittest.c
@@ -5,11 +5,16 @@
 #include <string.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <time.h>
 
 int main(int argc, char **argv)
 {
-    (void) argc;
-    (void) argv;
+    printf("args[%d]:", argc);
+    for(int i = 0; i < argc; i++)
+        printf(" %s", argv[i]);
+    printf("\n");
+
+    srand(time(NULL));
 
     int fd = open("/dev/random", O_RDONLY);
     if(fd < 0)
@@ -24,7 +29,10 @@ int main(int argc, char **argv)
         close(fd);
         return 1;
     }
-    printf("%d\n", data);
+    time_t tm = time(NULL);
+    char buf[128];
+    strftime(buf, 128, "%c", localtime(&tm));
+    printf("%s %d %d\n", buf, data, rand());
     close(fd);
     return 0;
 }
